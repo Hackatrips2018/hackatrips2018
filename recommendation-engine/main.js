c461e5d56b20f86f7b12e5aa6c955029fac81458
furstenheim <furstenheim@geoblink.com>
2018-01-20 15:36:12 +0100

    Get one hotel data

--- recommendation-engine/main.js
+++ recommendation-engine/main.js
@@ -4,29 +4,65 @@ const endpoint = 'http://sandbox.hotelscombined.com/api/2.0'
 const querystring = require('querystring')
 const r2 = require('r2')
 const ping = `${endpoint}/ping?${querystring.stringify({apiKey})}`
-
+module.exports = main
 main()
 
 async function main () {
   const result = await r2(ping).text
   console.log(result)
   const hotels = await search()
-  console.log(hotels)
+  // console.log(hotels)
+  const hotel = await getHotel(hotels.results[0].key)
+  const basic = await basicSearch()
+  // console.log(basic)
+  console.log(hotel)
+  return {hotel, hotels, basic}
 }
-
+// no distance
+async function basicSearch () {
+  const lat = 41.378737
+  const lon = 2.1741
+  const search = {
+    destination: `latlon:${lat},${lon}`,
+    checkin: '2018-11-03',
+    checkout: '2018-11-13',
+    rooms: ['2'],
+    apiKey,
+    pageIndex: 0,
+    sessionId: '1',
+  }
+  const url = `${endpoint}/hotels/basic?${querystring.stringify(search)}`
+  return r2(url).json
+}
 
 async function search () {
   const lat = 41.378737
   const lon = 2.1741
   const search = {
     destination: `latlon:${lat},${lon}`,
-    // checkin: '2018-11-03',
-    // checkout: '2018-11-13',
+    checkin: '2018-11-03',
+    checkout: '2018-11-13',
     rooms: 2,
     apiKey,
+    pageIndex: 0,
     sessionId: '1',
   }
   const url = `${endpoint}/hotels?${querystring.stringify(search)}`
   return r2(url).json
+}
 
+async function getHotel (key) {
+  const lat = 41.378737
+  const lon = 2.1741
+  const search = {
+    hotel: key,
+    checkin: '2018-11-03',
+    checkout: '2018-11-13',
+    rooms: 2,
+    apiKey,
+    pageIndex: 0,
+    sessionId: '1',
+  }
+  const url = `${endpoint}/hotel?${querystring.stringify(search)}`
+  return r2(url).text
 }
