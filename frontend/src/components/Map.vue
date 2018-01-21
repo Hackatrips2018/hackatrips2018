a61c70deb55c26ac9579baf6448f50a9ff44531f
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2018-01-21 12:59:35 +0100

    :lipstick: Update hotel markers

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -25,7 +25,7 @@
         :lat-lng="marker.coordinates"
         :icon="highlightedClusterPoisIcon"
       >
-        <v-popup :content="marker.name"></v-popup>
+        <v-popup :content="getPlaceMarkerContent(marker)"></v-popup>
       </v-marker>
       <v-geojson-layer
         v-if="showHotelCombinations && highlightedHotelCombination"
@@ -38,7 +38,9 @@
         :key="hotel.id"
         :lat-lng="getHotelLatLng(hotel)"
         :icon="hotelIcon"
-      />
+      >
+        <v-popup :content="getHotelMarkerContent(hotel)"></v-popup>
+      </v-marker>
     </v-map>
   </div>
 </template>
@@ -105,7 +107,7 @@ export default {
       return this.$store.state.highlightedPoiCluster
         ? this.$store.state.highlightedPoiCluster.elements.map(poi => {
           return {
-            name: poi.name,
+            poi: poi,
             coordinates: L.latLng(poi.latitude, poi.longitude)
           }
         })
@@ -127,9 +129,9 @@ export default {
     },
     hotelIcon () {
       return L.icon({
-        iconUrl: require('../assets/marker-icon-2x.png'),
-        iconSize: [25, 41],
-        iconAnchor: [12.5, 20]
+        iconUrl: require('../assets/marker-hotel-icon-2x.png'),
+        iconSize: [25, 25],
+        iconAnchor: [12.5, 12.5]
       })
     },
     hotelCombinations () {
@@ -182,14 +184,55 @@ export default {
     },
     getHotelLatLng (hotel) {
       return L.latLng(hotel.latitude, hotel.longitude)
+    },
+    getPlaceMarkerContent (marker) {
+      return `
+        <div class="place-marker-popup-content">
+          <div class="place-marker-popup-content__thumbnail" style="background: url(${marker.poi.picture_url})"></div>
+          <strong class="place-marker-popup-content__title">${marker.poi.name}</strong>
+        </div>
+      `
+    },
+    getHotelMarkerContent (hotel) {
+      let htmlForStars = ``
+      for (let i = 0; i < hotel.starRating; i++) {
+        htmlForStars += `<svg aria-hidden="true" data-fa-processed="" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="star svg-inline--fa fa-star fa-w-18 fa-fw fa-sm"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" class=""></path></svg>`
+      }
+
+      return `
+        <div class="place-hotel-popup-content">
+          <strong class="place-hotel-popup-content__title">${hotel.name}</strong>
+          <br />
+          ${htmlForStars} <em class="place-hotel-popup-content__title">${hotel.lowestRate} €</em>
+        </div>
+      `
     }
   }
 }
 </script>
 
 <style>
+
 #map {
   width: 100vw;
   height: 100vh;
 }
+
+.place-marker-popup-content {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.place-marker-popup-content__thumbnail {
+  width: 50px;
+  height: 50px;
+  background-size: cover;
+  display: block;
+}
+
+.place-marker-popup-content__title {
+  margin-left: 10px;
+}
+
 </style>
